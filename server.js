c1424a33cae8dd0930ce43e3fa84fb93d4702750
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-09-26 14:30:10 +0900

    feat: 기본 라우터 설정 완료

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const port = process.env.serverPort
-
 const nunjucks = require("nunjucks")
+
+const router = require("./src/index")
 
 app.set("view engine", "html")
 nunjucks.configure({
     express: app
 })
 
+app.use(router)
 
 
 
