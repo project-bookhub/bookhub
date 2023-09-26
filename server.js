6f562bf9ac9af152a7fcdb1cd13544c19981e63d
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-09-26 17:01:31 +0900

    feat: cookieParser, bodyParser, authMiddleware 구현

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 require("dotenv").config()
 const port = process.env.serverPort
 const nunjucks = require("nunjucks")
+const cookieParser = require("cookie-parser")
+const authMiddleware = require("./src/middlewares/jwtAuthMiddleware")
 
 const router = require("./src/index")
 
@@ -11,11 +13,19 @@ nunjucks.configure({
     express: app
 })
 
+
+app.use(cookieParser())
+app.use(express.urlencoded({extended: true}))
+app.use(authMiddleware.auth)
+
 app.use(router)
 app.use((err, req, res, next) => {
 
-    // 4444: DB error
-    if (err.message === "4444") res.status(500).send("DB Error!")
+    // 5000: DB error
+    if (err.message === "5000") res.status(500).send("DB Error!")
+
+    // 4000: 인증 에러
+    if (err.message === "4000") res.status(500).send("인증 Error!")
 
 });
 
