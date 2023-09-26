0fe5c808952c9077743ffa0cdae9df8eea7f3b5d
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-09-26 14:27:29 +0900

    feat: Template Engine, nunjucks 설정 완료

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express()
 require("dotenv").config()
 const port = process.env.serverPort
 
+const nunjucks = require("nunjucks")
+
+app.set("view engine", "html")
+nunjucks.configure({
+    express: app
+})
+
 
 
 
