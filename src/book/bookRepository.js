989cce97883d0e717a3bdc56d1ce4736bb8deff9
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-09-27 17:35:52 +0900

    fix: book_writer fk 값 -> user id 변경

--- src/book/bookRepository.js
+++ src/book/bookRepository.js
@@ -2,9 +2,15 @@ const pool = require("../../pool");
 
 exports.findAllBook = async () => {
   try {
-    const sql1 = "SELECT * FROM book ORDER BY book_likes DESC";
+    const sql1 =
+      "SELECT book.*, user.user_id AS book_writer " +
+      "FROM book JOIN user ON book.book_writer = user.user_uid ORDER BY book_likes DESC;";
+
     const [orderByLikes] = await pool.query(sql1);
-    const sql2 = "SELECT * FROM book ORDER BY book_created_at DESC";
+
+    const sql2 =
+      "SELECT book.*, user.user_id AS book_writer " +
+      "FROM book JOIN user ON book.book_writer = user.user_uid ORDER BY book_created_at DESC;";
     const [orderByDate] = await pool.query(sql2);
 
     const result = [orderByLikes, orderByDate];
