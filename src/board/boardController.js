79f7f22001abd8c6acbd588a1bbd0c7bf6515ea3
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-10-01 20:12:06 +0900

    feat: board 기본 구조 생성

---
+++ src/board/boardController.js
@@ -0,0 +1,1 @@
+const boardService = require("./boardService");
---
+++ src/board/boardRoute.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const boardRouter = express.Router();
+const boardController = require("./boardController");
+
+module.exports = boardRouter;
