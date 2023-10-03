c58bc857f7556401d1e97e8c73e2dab408b76fe7
subin <github@subin.kr>
2023-10-03 17:24:40 +0900

    fix: ui 수정

--- views/public/js/index.js
+++ views/public/js/index.js
@@ -17,20 +17,24 @@ const img = new Image();
 const imageNameArr = ["1", "2", "3", "4", "5"];
 let imageIdx = 0;
 
-slide.innerHTML = `<div class="image-wrapper"><img class="slide-image-${imageIdx}" src="image/${imageNameArr[imageIdx]}.png" /></div>`;
-createCircles();
-
 function createCircles() {
   slide.innerHTML += `<div class="circle-area"></div>`;
   const circleArea = document.querySelector(".circle-area");
   for (let i = 0; i < imageNameArr.length; i++) {
     if (imageIdx === i) {
-      circleArea.innerHTML += `<div class="highlight-circle">⬤</div>`;
+      circleArea.innerHTML += `<div class="highlight-circle">&nbsp;⬤&nbsp;</div>`;
     } else {
-      circleArea.innerHTML += `<div class="circle">⬤</div>`;
+      circleArea.innerHTML += `<div class="circle">&nbsp;⬤&nbsp;</div>`;
     }
   }
 }
+
+function slideImage(idx) {
+  slide.innerHTML = `<div class="image-wrapper"><img class="slide-image-${idx}" src="image/${imageNameArr[idx]}.png" /></div>`;
+  createCircles();
+}
+
+slideImage(imageIdx);
 
 slide.onmouseover = () => {
   let befXPos = 0;
@@ -38,32 +42,39 @@ slide.onmouseover = () => {
 
   document.onmousedown = (e) => {
     befXPos = e.pageX;
-    console.log(e.pageX);
+    // console.log(e.pageX);
   };
   document.onmouseup = (e) => {
     aftXPos = e.pageX;
-    console.log(e.pageX);
+    // console.log(e.pageX);
     if (befXPos < aftXPos) {
       imageIdx--;
       if (imageIdx < 0) {
         imageIdx = imageNameArr.length - 1;
       }
-      slide.innerHTML = `<div class="image-wrapper"><img class="slide-image-${imageIdx}" src="image/${imageNameArr[imageIdx]}.png" /></div>`;
+      slideImage(imageIdx);
       befXPos = aftXPos = 0;
     } else if (befXPos > aftXPos) {
       imageIdx++;
       if (imageIdx > imageNameArr.length - 1) {
         imageIdx = 0;
       }
-      slide.innerHTML = `<div class="image-wrapper"><img class="slide-image-${imageIdx}" src="image/${imageNameArr[imageIdx]}.png" /></div>`;
+      slideImage(imageIdx);
       befXPos = aftXPos = 0;
     } else {
-      slide.innerHTML = `<div class="image-wrapper"><img class="slide-image-${imageIdx}" src="image/${imageNameArr[imageIdx]}.png" /></div>`;
+      slideImage(imageIdx);
     }
-    createCircles();
   };
 };
 slide.onmouseout = () => {
   document.onmousedown = () => {};
   document.onmouseup = () => {};
 };
+
+setInterval(() => {
+  imageIdx++;
+  if (imageIdx > imageNameArr.length - 1) {
+    imageIdx = 0;
+  }
+  slideImage(imageIdx);
+}, 5000);
